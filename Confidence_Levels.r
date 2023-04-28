c3fe89119b065cd8730ba8541de70159da4ca6de
Sunita Bhattacharya <33515885+sunitab55@users.noreply.github.com>
2023-04-28 03:23:31 -0700

    Update Confidence_Levels.r

--- Confidence_Levels.r
+++ Confidence_Levels.r
@@ -54,3 +54,25 @@ p - 1.96 * se
 # Which has the widest range?
 99
 
+# Assign the standard deviation squared to new object "ss"
+ss <- (1.25)^2
+
+# Assign the value of the Z-score squared to new object "zs"
+zs <- (1.96)^2
+
+# Assign the value of the margin of error squared to the new object "ms"
+ms <- (0.2)^2
+
+# Calculate the neccessary sample size
+(ss * zs)/ms
+
+# Assign the value of p(1-p) to object "p"
+p <- (1 - 0.5) * 0.5
+# Assign the value of the Z-score square to new object "z"
+z <- 1.96
+
+# Assign the value of the margin of error squared to the new object "ms"
+ms <- (0.05)^2
+
+# Calculate the neccessary sample size
+(p * z * z)/ms
